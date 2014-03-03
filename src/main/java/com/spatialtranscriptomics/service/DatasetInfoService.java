65c92996fd3b8fe2d4677d9327c59d0cae86df99
Joel Sjöstrand <joel.sjostrand@scilifelab.se>
2014-03-03 14:55:40 +0100

    version changes, etc

---
+++ src/main/java/com/spatialtranscriptomics/service/DatasetInfoService.java
@@ -0,0 +1,30 @@
+/*
+*Copyright © 2012 Spatial Transcriptomics AB
+*Read LICENSE for more information about licensing terms
+*Contact: Jose Fernandez Navarro <jose.fernandez.navarro@scilifelab.se>
+* 
+*/
+
+package com.spatialtranscriptomics.service;
+
+import java.util.List;
+
+import com.spatialtranscriptomics.model.DatasetInfo;
+
+public interface DatasetInfoService {
+
+	public DatasetInfo find(String id);
+
+	public List<DatasetInfo> list();
+	
+	public List<DatasetInfo> listForAccount(String accountId);
+	
+	public List<DatasetInfo> listForDataset(String datasetId);
+
+	public DatasetInfo add(DatasetInfo datasetinfo);
+
+	public void update(DatasetInfo datasetinfo);
+
+	public void delete(String id);
+
+}
